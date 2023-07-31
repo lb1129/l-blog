a323f54f1dae6072de681ab155e48f975219c674
leiboT <superviho1@gmail.com>
2023-07-31 19:23:40 +0800

    chore: add sidebar

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -28,19 +28,95 @@ export default defineConfig({
   themeConfig: {
     nav: [
       { text: "首页", link: "/" },
-      { text: "博客", link: "/blog/api-examples", activeMatch: "/blog/" },
+      {
+        text: "博客",
+        link: "/blog/vue/change-detection",
+        activeMatch: "/blog/",
+      },
     ],
 
     socialLinks: [{ icon: "github", link: "https://github.com/lb1129" }],
 
     sidebar: [
       {
-        text: "示例",
+        text: "vue",
         collapsed: false,
         items: [
-          { text: "Runtime API Examples", link: "/blog/api-examples" },
-          { text: "Markdown Examples", link: "/blog/markdown-examples" },
+          { text: "变更检查机制", link: "/blog/vue/change-detection" },
+          { text: "依赖注入", link: "/blog/vue/dependency-injection" },
+          { text: "http请求", link: "/blog/vue/http" },
+          { text: "国际化", link: "/blog/vue/internationalization" },
+          { text: "jsx", link: "/blog/vue/jsx" },
+          { text: "keep-alive", link: "/blog/vue/keep-alive" },
+          { text: "路由", link: "/blog/vue/router" },
+          { text: "数据仓库", link: "/blog/vue/store" },
+          { text: "模板语法", link: "/blog/vue/template" },
         ],
+      },
+      {
+        text: "react",
+        collapsed: false,
+        items: [
+          { text: "变更检查机制", link: "/blog/react/change-detection" },
+          { text: "依赖注入", link: "/blog/react/dependency-injection" },
+          { text: "http请求", link: "/blog/react/http" },
+          { text: "国际化", link: "/blog/react/internationalization" },
+          { text: "jsx", link: "/blog/react/jsx" },
+          { text: "keep-alive", link: "/blog/react/keep-alive" },
+          { text: "路由", link: "/blog/react/router" },
+          { text: "数据仓库", link: "/blog/react/store" },
+        ],
+      },
+      {
+        text: "angular",
+        collapsed: false,
+        items: [
+          { text: "变更检查机制", link: "/blog/angular/change-detection" },
+          { text: "依赖注入", link: "/blog/angular/dependency-injection" },
+          { text: "http请求", link: "/blog/angular/http" },
+          { text: "国际化", link: "/blog/angular/internationalization" },
+          { text: "keep-alive", link: "/blog/angular/keep-alive" },
+          { text: "路由", link: "/blog/angular/router" },
+          { text: "数据仓库", link: "/blog/angular/store" },
+          { text: "模板语法", link: "/blog/angular/template" },
+        ],
+      },
+      {
+        text: "uni-app",
+        collapsed: false,
+        items: [
+          { text: "国际化", link: "/blog/uni-app/internationalization" },
+          { text: "数据仓库", link: "/blog/uni-app/store" },
+          { text: "主题色", link: "/blog/uni-app/theme" },
+        ],
+      },
+      {
+        text: "uni-clound",
+        collapsed: false,
+        items: [{ text: "url化", link: "/blog/uni-clound/urlization" }],
+      },
+      {
+        text: "webpack",
+        collapsed: false,
+        items: [
+          { text: "loader", link: "/blog/webpack/loader" },
+          { text: "plugin", link: "/blog/webpack/plugin" },
+          { text: "env", link: "/blog/webpack/env" },
+          { text: "dev-server", link: "/blog/webpack/dev-server" },
+        ],
+      },
+      {
+        text: "ci-cd",
+        collapsed: false,
+        items: [
+          { text: "代码检查校验", link: "/blog/ci-cd/code-lint" },
+          { text: "gh-pages", link: "/blog/ci-cd/gh-pages" },
+        ],
+      },
+      {
+        text: "common",
+        collapsed: false,
+        items: [{ text: "订阅发布", link: "/blog/common/pubsub" }],
       },
     ],
 
@@ -70,12 +146,12 @@ export default defineConfig({
     },
 
     lastUpdated: {
-      text: '上次更新'
+      text: "上次更新",
     },
 
     docFooter: {
-      prev: '上一篇',
-      next: '下一篇'
+      prev: "上一篇",
+      next: "下一篇",
     },
 
     footer: {
